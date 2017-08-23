2204e8745af18985f6c88e241f67631eb4605d8e
Israel <arnulfo_israel@hotmail.com>
2017-08-23 16:48:33 -0500

    Added BasicUserStore Test

---
+++ test/Unosquare.Swan.AspNetCore.Test/BasicUserStoreTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Unosquare.Swan.AspNetCore.Models;
+
+namespace Unosquare.Swan.AspNetCore.Test
+{
+    [TestClass]
+    public class BasicUserStoreTest
+    {
+        private readonly CancellationToken cancellatinToken = new CancellationToken();
+        private readonly ApplicationUser _user = new ApplicationUser
+        {
+            UserId = "1",
+            UserName = "Israel",
+            Email = "myemail@domain.com",
+            PhoneNumber = "1234567890",
+            PhoneNumberConfirmed = true,
+            PasswordHash = "PasswordHash",
+            TwoFactorEnabled = false
+        };
+
+        [TestMethod]
+        public void CreateAsyncTest()
+        {
+            var userStore = new BasicUserStore();
+
+            var result = userStore.CreateAsync(_user, cancellatinToken);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.IsCompleted);
+        }
+
+    }
+}
