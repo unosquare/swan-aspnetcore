9513d294badf2ea6ed2eb217a31e2538f01f962f
Geo Perez <geovanni.perez@gmail.com>
2018-08-29 15:46:27 -0500

    Update deps

--- src/Unosquare.Swan.AspNetCore.Sample/Startup.cs
+++ src/Unosquare.Swan.AspNetCore.Sample/Startup.cs
@@ -6,7 +6,6 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
     using Microsoft.Extensions.Logging;
     using Microsoft.IdentityModel.Tokens;
     using System;
